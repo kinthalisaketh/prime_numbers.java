56f54a48ef72a3daf28a7875f5442a05b50a89d5
Kinthali Saketh <103805011+kinthalisaketh@users.noreply.github.com>
2023-09-28 19:21:17 +0530

    Create prime_number.java

---
+++ prime_number.java
@@ -0,0 +1,32 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int n = 50; // Define the range up to which you want to find prime numbers
+
+        System.out.println("Prime numbers between 1 and " + n + ":");
+        
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Function to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num <= 3) {
+            return true;
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false;
+        }
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
